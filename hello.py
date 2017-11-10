ab7c5633959b4d3f85e64db4cac00fc12bd68881
root <wizlif.144@gmail.com>
2017-11-10 20:56:46 +0300

    api

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
 import os
 import sys
+
+import simplejson as simplejson
 from django.conf import settings
 
 DEBUG = os.environ.get('DEBUG', 'on') == 'on'
@@ -22,12 +24,40 @@ from django.conf.urls import url
 from django.core.wsgi import get_wsgi_application
 from django.http import HttpResponse
 
+import couchdb
+
+couchserver = couchdb.Server("http://localhost:5984/")
+
+dbname = "ezyextension"
+db = None
+
+if dbname in couchserver:
+    db = couchserver[dbname]
+
+
 def index(request):
     return HttpResponse('Hello World')
 
 
+def ordersV1(request):
+    if request.method == 'GET':
+        data = []
+        for x in db.view('orders/order_total', include_docs=True):
+            data.append(x["doc"])
+        data = simplejson.dumps(data)
+        return HttpResponse(data, content_type='application/json')
+
+    elif request.method == 'POST':
+        data = []
+        for x in db.view('orders/all', include_docs=True):
+            data.append(x["doc"])
+        data = simplejson.dumps(data)
+        return HttpResponse(data, content_type='application/json')
+
+
 urlpatterns = (
     url(r'^$', index),
+    url(r'^api/v1/orders', ordersV1),
 )
 
 application = get_wsgi_application()
